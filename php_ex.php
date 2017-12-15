9d035ace7aa4dc89f64bed31bbb31d7d83903b45
lethisa <lethisaputri@gmail.com>
2017-12-15 14:13:39 +0700

    add

---
+++ php_ex.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <?php
+    // Variable
+    $name = "John Doe";
+    $number = 100;
+    echo $name.' '.$number;
+
+    $names = '<h1>Marry</h1>';
+    echo $names;
+    ?>
+     <div>
+       <a>Here is</a>
+     </div>
+
+    <?php
+    echo 'Ini adalah sebuah penyelesaian !';
+    ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+  </body>
+</html>
