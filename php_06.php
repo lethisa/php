9ab5c6ac84370eeaa8410744be8df94e3fbec118
lethisa <lethisaputri@gmail.com>
2017-12-21 15:09:19 +0700

    add #1

---
+++ php_06.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Form Input</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="col-xs-6">
+      <form action="php_06_proc.php" method="post">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" name="username" class="form-control" placeholder="Enter Username" />
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" name="password" class="form-control" placeholder="password" />
+        </div>
+
+        <input class="btn-primary" type="submit" name="submit" value="SUBMIT" />
+      </form>
+    </div>
+  <!-- container end -->
+  </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ php_06_proc.php
@@ -0,0 +1,27 @@
+<?php
+
+  // submit action
+  if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $name = array('Edwin','John','Marrie','Jane','Tom');
+    $minimum = 5;
+    $maximum = 10;
+
+    // username length check
+    if (strlen($username) < $minimum) {
+      echo 'username too short';
+    } elseif (strlen($username) > $maximum) {
+      echo 'username too long';
+    }
+
+    // username check available
+    if (!in_array($username, $name)) {
+      echo 'not exist';
+    } else {
+      echo 'welcome';
+    }
+}
+
+?>
