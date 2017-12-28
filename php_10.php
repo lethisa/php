858c9bdb10a20a916eab72e0829e60710b7bf6b6
lethisa <lethisaputri@gmail.com>
2017-12-28 14:17:57 +0700

    add

--- php_10.php
+++ php_10.php
@@ -1,6 +1,13 @@
 <?php
 
-$_COOKIE;
+  //$_COOKIE();
+
+  $name = "someName";
+  $value = 100;
+  $expiration = time() +(60*6*24*7); // second - minutes - hours - date
+  setcookie($name, $value, $expiration);
+
+
 
 ?>
 
@@ -16,6 +23,18 @@ $_COOKIE;
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
   </head>
   <body>
+
+    <?php
+
+    // check cookies
+    if (isset($_COOKIE["someName"])) {
+        $someOne = $_COOKIE["someName"];
+        echo $someOne;
+    } else {
+      $someOne = " ";
+    }
+
+    ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
