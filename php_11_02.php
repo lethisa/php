6bf57afba7f03061ef1c093e015226c3b3300d60
lethisa <lethisaputri@gmail.com>
2017-12-28 14:59:45 +0700

    add

--- php_11_02.php
+++ php_11_02.php
@@ -1,16 +1,9 @@
 <?php
+session_start();
 
-  //$_COOKIE();
-
-  $name = "someName";
-  $value = 100;
-  $expiration = time() +(60*6*24*7); // second - minutes - hours - date
-  setcookie($name, $value, $expiration);
-
-
+echo $_SESSION['greeting'];
 
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -18,23 +11,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>COOKIES</title>
+    <title>SESSION 2</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
   </head>
   <body>
 
-    <?php
 
-    // check cookies
-    if (isset($_COOKIE["someName"])) {
-        $someOne = $_COOKIE["someName"];
-        echo $someOne;
-    } else {
-        $someOne = " ";
-    }
-
-    ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
