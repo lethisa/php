dd5c72a35b3c12d14f837b199ceb851da767a126
lethisa <lethisaputri@gmail.com>
2018-01-02 12:34:42 +0700

    add

--- cms/include/navigation.php
+++ cms/include/navigation.php
@@ -1,3 +1,6 @@
+<!-- Coonection -->
+<?php include "include/db.php"; ?>
+
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
@@ -14,18 +17,22 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
+                <?php
+                // categories query
+                $query = "SELECT * FROM categories";
+                $select_categories = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_array($select_categories)) {
+                    $cat_title = $row['cat_title'];
+
+                    echo "<li><a href='#'>{$cat_title}</a></li>";
+                }
+                ?>
             </ul>
+
         </div>
         <!-- /.navbar-collapse -->
     </div>
     <!-- /.container -->
 </nav>
+<!-- /.nav -->
