9decaf7ac0949ec14df4981775acc97f5d142723
lethisa <lethisaputri@gmail.com>
2018-01-07 18:14:01 +0700

    add

---
+++ cms/admin/js/scripts.js
@@ -0,0 +1,12 @@
+
+  $('#selectAllBoxes').click(function(event) {
+    if (this.checked) {
+      $('.checkBoxes').each(function() {
+        this.checked = true;
+      });
+    } else {
+      $('.checkBoxes').each(function() {
+        this.checked = false;
+      })
+    };
+  })
